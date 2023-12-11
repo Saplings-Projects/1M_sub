be7d22c0c5b0e5079634b43d7d139a38adfc89aa
Turtyo <49365680+Turtyo@users.noreply.github.com>
2023-12-11 20:02:17 +0100

    Modified some forgotten instances of buff to status

--- Status/Debuffs/Debuff_Poison.gd
+++ Status/Debuffs/Debuff_Poison.gd
@@ -7,7 +7,7 @@ func on_turn_start() -> void:
 	var damage_data: DealDamageData = DealDamageData.new()
 	damage_data.damage = status_power
 	damage_data.caster = status_applier
-	damage_data.ignore_caster_buffs = true
-	damage_data.ignore_target_buffs = true
+	damage_data.ignore_caster_status = true
+	damage_data.ignore_target_status = true
 	
 	status_owner.get_health_component().deal_damage(damage_data)
