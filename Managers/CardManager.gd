25bb06f865585707a7cea98999fb5f8c3445303f
Seth Burchfield <cheesefrycook@gmail.com>
2023-12-14 09:39:38 -0600

    Add clarifying comment in CardManager

--- Managers/CardManager.gd
+++ Managers/CardManager.gd
@@ -7,6 +7,8 @@ signal on_card_container_initialized
 var card_container: CardContainer = null
 
 
+# Call this from CardContainer to initialize. This allows you to get the current CardContainer from
+# anywhere by calling CardManager.card_container
 func set_card_container(in_card_container: CardContainer) -> void:
 	card_container = in_card_container
 	on_card_container_initialized.emit()
