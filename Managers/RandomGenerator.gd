66a17d4150cb263417b1c695a8fa1c26fe24dac9
Seth Burchfield <cheesefrycook@gmail.com>
2023-12-14 11:29:29 -0600

    Fix shadowed function in RandomGenerator

--- Managers/RandomGenerator.gd
+++ Managers/RandomGenerator.gd
@@ -6,10 +6,10 @@ const GAME_SEED = -1
 
 
 func _ready() -> void:
-	set_seed(GAME_SEED)
+	set_game_seed(GAME_SEED)
 
 
-func set_seed(given_seed: int = -1) -> void:
+func set_game_seed(given_seed: int = -1) -> void:
 	var set_seed: int = given_seed
 	if set_seed == -1:
 		set_seed = randi()
