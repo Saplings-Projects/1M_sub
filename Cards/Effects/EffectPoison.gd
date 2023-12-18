5b39fce3296cb3e9b82b671198451b44d92b5371
Turtyo <49365680+Turtyo@users.noreply.github.com>
2023-12-19 00:09:11 +0100

    Made the EffectPoison to have a reference for other Status effects

--- Cards/Effects/EffectPoison.gd
+++ Cards/Effects/EffectPoison.gd
@@ -1,7 +1,8 @@
 class_name EffectPoison extends EffectBase
 
 # @Override
-@warning_ignore("unused_parameter")
 func apply_effect(caster: Entity, target:Entity, value: int) -> void:
-    # implemented in future push
-    pass
+    var debuff_poison: Debuff_Poison = Debuff_Poison.new() 
+    # if not instanciated, the debuff is not seen as a child of StatusBase
+    debuff_poison.status_turn_duration = value
+    _apply_status(debuff_poison, caster, target)
