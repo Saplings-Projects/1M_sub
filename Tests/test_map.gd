eff1e0b2943b158497478ad3a7eafc9ccd8ccfb5
multi arm <cylendraki@gmail.com>
2023-12-18 20:22:18 -0500

    Last second change
    Fixed some formatting stuff

--- Tests/test_map.gd
+++ Tests/test_map.gd
@@ -1,10 +1,11 @@
-extends GutTest
+extends GutTest ## Tests for MapManager
 
+var test_generator = load("res://Map/MapManager.gd")
 
+var test_floor:Floor = Floor.new()
 
 func test_map_gen():
-	var test_generator = load("res://Map/MapManager.gd")
-	var test_floor:Floor = Floor.new()
+	
 	var result = test_generator.generate_rooms(test_floor)
 	assert_eq(result, test_floor, "Generator should return floor class")
 	pass
