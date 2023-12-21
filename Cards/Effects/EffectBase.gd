c028d3439b8eba2750ab6c4f8186dc2162c42054
Turtyo <49365680+Turtyo@users.noreply.github.com>
2023-12-21 01:21:08 +0100

    Changed EffectData to prevent an init bug | Added basic targeting function

--- Cards/Effects/EffectBase.gd
+++ Cards/Effects/EffectBase.gd
@@ -5,6 +5,12 @@ class_name EffectBase extends Resource
 func apply_effect(caster: Entity, target: Entity, value: int) -> void:
     pass
 
+# the goal of this function is to provide a base targeting
+# here it's not really useful for what is does, but the goal is to return differently
+# in children class. For example an AoE where you return the entity and the entities on its side
+func build_target_list(entity: Entity) -> Array[Entity]:
+    return [entity]
+
 # To be called by child classes through apply_effect. But since there might be multiple effects that need it, it's available in the parent class
 func _apply_status(status: StatusBase, caster: Entity, target: Entity) -> void:
     target.get_status_component().add_status(status, caster)
--- Cards/Effects/EffectData.gd
+++ Cards/Effects/EffectData.gd
@@ -2,21 +2,20 @@ class_name EffectData extends Resource
 
 # Effect data is a wrapper for the Effect class. It contains an effect, a caster, a target and a value (which will be applied by the effect)
 
-var effect: EffectBase = null
+@export var effect: EffectBase = null
 var caster: Entity = null
 var list_targets: Array[Entity] = []
-var value: int = 0
+@export var value: int = 0
 
-func _init(_effect: EffectBase, _caster: Entity, _list_targets: Array[Entity], _value: int) -> void:
-    self.effect = _effect
-    self.caster = _caster
-    self.list_targets = _list_targets
-    self.value = _value
+func _init(_effect: EffectBase = null, _caster: Entity = null, _list_targets: Array[Entity] = [], _value: int = 0) -> void:
+	self.effect = _effect
+	self.caster = _caster
+	self.list_targets = _list_targets
+	self.value = _value
 
-func add_effect_data(card: CardBase, _effect: EffectBase, _caster: Entity, _list_targets: Array[Entity], _value: int) -> void:
-    card.card_effects_data.append(EffectData.new(_effect, _caster, _list_targets, _value))
-
-func apply_effect_data() -> void:
-    # Help function to call more easily from the card point of view
-    for target: Entity in list_targets:
-        self.effect.apply_effect(self.caster, target, self.value)
+func apply_effect_data(entity: Entity) -> void:
+	# Help function to call more easily from the card point of view
+	list_targets = effect.build_target_list(entity)
+	for target: Entity in list_targets:
+		self.effect.apply_effect(self.caster, target, self.value)
+	list_targets = []
