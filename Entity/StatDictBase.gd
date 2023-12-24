ea12d7c8e1b7499793fef2486bb5406c87df7eaa
Turtyo <49365680+Turtyo@users.noreply.github.com>
2023-12-24 17:03:06 +0100

    Added some more modifiers to the POSSIBLE_MODIFIERS to show versatility of use

--- Entity/StatDictBase.gd
+++ Entity/StatDictBase.gd
@@ -1,9 +1,11 @@
 class_name StatDictBase extends Resource
 
-# TODO move to a global const file (which is already being made in another PR, so I'm not creating in double for now)
+# TODO move to a global const file (which is already being made in another PR (#30), so I'm not creating in double for now)
 const POSSIBLE_MODIFIERS: Array[String] = [
     "damage",
-    "poison"
+    "poison",
+    "draw",
+    "card_reward_number"
 ]
 
 var stat_dict: Dictionary = {}
