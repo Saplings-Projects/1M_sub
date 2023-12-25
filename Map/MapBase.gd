c11cdfab4e7b189fa6d20e58df66846ed2d5787c
multi arm <cylendraki@gmail.com>
2023-12-24 21:10:10 -0500

    Feedback changes: review edition
    Changed create_map loop order
    Changed create_map append to assign room type instead of placeholder
    Changed map width to reflect shape rules

--- Map/MapBase.gd
+++ Map/MapBase.gd
@@ -5,4 +5,4 @@ class_name MapBase
 ## Holds basic functionality for a map
 
 @export var rooms: Array = [RoomBase] ## 2D array of rooms on the map. this will be assigned when generating rooms
-@export var floors_width: PackedInt32Array = [2,3,2] ## array for variable map width, each element is the width of a floor
+@export var floors_width: PackedInt32Array = [1, 3, 5, 9, 5, 3, 1] ## array for variable map width, each element is the width of a floor
--- Map/MapManager.gd
+++ Map/MapManager.gd
@@ -4,14 +4,16 @@ extends Node2D
 static func create_map(): ## Generates and Populates a map with rooms that have random room types. More in depth algorithms will be added in the future
 	var _map: MapBase = MapBase.new()
 	var _grid: Array = [] ## 2d array to return. this will be populated with rooms
-	for index_width: int in range(_map.floors_width.size()): ## loop through elements of the grid and assign a room type
+	
+	for index_height: int in range(_map.floors_width.size()): ## loop through elements of the grid and assign a room type
 		_grid.append([])
-		for index_height: int in range(_map.floors_width[index_width]):
-			_grid[index_width].append(0)
-			var _generated_room: RoomBase = RoomBase.new()
+		for index_width: int in range(_map.floors_width[index_height]):
 			var _rand_type_index: int = randi() % GlobalVar.EVENTS_CLASSIFICATION.size()
 			var _room_event: EventBase = GlobalVar.EVENTS_CLASSIFICATION[_rand_type_index].new()
+			var _generated_room: RoomBase = RoomBase.new()
 			_generated_room.room_event = _room_event
-			_grid[index_width][index_height] = _generated_room
+			_grid[index_height].append(_generated_room)
+	
 	_map.rooms = _grid
+	print(_map.rooms)
 	return _map
