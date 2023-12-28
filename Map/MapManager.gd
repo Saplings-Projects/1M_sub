062e93b58a0ad59e9261705ede748ba3aeed94b7
multi arm <cylendraki@gmail.com>
2023-12-28 03:58:58 -0500

    Static typing fixes
    added static typing where there was not any

--- Map/MapManager.gd
+++ Map/MapManager.gd
@@ -5,20 +5,21 @@ static func create_map(): ## Generates and Populates a map with rooms that have 
 	var _map: MapBase = MapBase.new()
 	var _grid: Array = [] ## 2d array to return. this will be populated with rooms
 	
-	for index_height: int in range(_map.floors_width.size()): ## loop through elements of the grid and assign a room type
-		var _max_floor_size = _map.floors_width[_map.floors_width.size()/2]
+	for index_height: int in range(_map.floors_width.size()): ## loop through the floor and append null to create a square grid
 		_grid.append([])
-		var _padding = (_max_floor_size - _map.floors_width[index_height])/2
-		var _floor_width = _map.floors_width[index_height]
+		var _max_floor_size: int = _map.floors_width[_map.floors_width.size()/2]
+		var _padding: int = (_max_floor_size - _map.floors_width[index_height])/2
+		var _floor_width: int = _map.floors_width[index_height]
 		for index_padding:int in _padding*2:
 			if _padding > 0:
 				_grid[index_height].insert(index_padding, null)
 		
-		for index_width: int in range(_map.floors_width[index_height]):
+		for index_width: int in range(_map.floors_width[index_height]):## loop through elements of the grid and assign a room type
 			var _rand_type_index: int = randi() % GlobalVar.EVENTS_CLASSIFICATION.size()
 			var _room_event: EventBase = GlobalVar.EVENTS_CLASSIFICATION[_rand_type_index].new()
 			var _generated_room: RoomBase = RoomBase.new()
 			_generated_room.room_event = _room_event
 			_grid[index_height].insert(index_width+_padding,_generated_room)
+	#print(_grid)
 	_map.rooms = _grid
 	return _map
