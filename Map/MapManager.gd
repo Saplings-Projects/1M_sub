f0d825b1908a374f3357a79806efc1f90fe65510
multi arm <cylendraki@gmail.com>
2023-12-28 03:46:18 -0500

    Changed map generation to be more accurate to references
    create_map() now generates to reflect the map shape reference (https://github.com/Saplings-Projects/1M_sub/issues/6#issue-2027403379)(https://github.com/Saplings-Projects/1M_sub/pull/30#issuecomment-1868931059)
    
    changed floors_width to reflect basic math

--- Map/MapManager.gd
+++ Map/MapManager.gd
@@ -6,14 +6,19 @@ static func create_map(): ## Generates and Populates a map with rooms that have 
 	var _grid: Array = [] ## 2d array to return. this will be populated with rooms
 	
 	for index_height: int in range(_map.floors_width.size()): ## loop through elements of the grid and assign a room type
+		var _max_floor_size = _map.floors_width[_map.floors_width.size()/2]
 		_grid.append([])
+		var _padding = (_max_floor_size - _map.floors_width[index_height])/2
+		var _floor_width = _map.floors_width[index_height]
+		for index_padding:int in _padding*2:
+			if _padding > 0:
+				_grid[index_height].insert(index_padding, null)
+		
 		for index_width: int in range(_map.floors_width[index_height]):
 			var _rand_type_index: int = randi() % GlobalVar.EVENTS_CLASSIFICATION.size()
 			var _room_event: EventBase = GlobalVar.EVENTS_CLASSIFICATION[_rand_type_index].new()
 			var _generated_room: RoomBase = RoomBase.new()
 			_generated_room.room_event = _room_event
-			_grid[index_height].append(_generated_room)
-	
+			_grid[index_height].insert(index_width+_padding,_generated_room)
 	_map.rooms = _grid
-	print(_map.rooms)
 	return _map
