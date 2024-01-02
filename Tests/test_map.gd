af51f129f1c9c62c26870df03b98487e2d7da2cb
multi arm <cylendraki@gmail.com>
2024-01-02 05:23:27 -0500

    Fixed tests
    fixed unintended test changes

--- Tests/test_map.gd
+++ Tests/test_map.gd
@@ -2,20 +2,12 @@ extends GutTest ## Tests for MapManager, more will be added in the future
 
 var test_generator = load("res://Map/MapManager.gd")
 
-
-
 func test_map_gen():
 	var test_width: Array[int] = [1,3,5,3,1]
 	var test_map: MapBase = test_generator.create_map(test_width)
-	assert_null(test_map.rooms[0][0])
-	assert_null(test_map.rooms[0][1])
-	assert_null(test_map.rooms[0][3])
-	assert_null(test_map.rooms[0][4])
-	assert_null(test_map.rooms[4][0])
-	assert_null(test_map.rooms[4][1])
-	assert_null(test_map.rooms[4][3])
-	assert_null(test_map.rooms[4][4])
-	assert_null(test_map.rooms[1][0])
-	assert_null(test_map.rooms[1][4])
-	assert_null(test_map.rooms[3][0])
-	assert_null(test_map.rooms[3][4])
+	for index_height in test_map.rooms.size():
+		var _padding_loop : int = (5 - test_width[index_height])/2
+		var _padding_size : int = (5 + test_width[index_height])/2
+		for index_padding in _padding_loop:
+			assert_null(test_map.rooms[index_height][index_padding])
+			assert_null(test_map.rooms[index_height][index_padding+_padding_size])
