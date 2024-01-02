14ce1f67149d7f14f27957b12e5655f15df32bc5
multi arm <cylendraki@gmail.com>
2024-01-02 03:22:43 -0500

    Feedback changes
    Static typing changes
    Added create_map return type
    
    changed map generation (optimization)
    
    (MapBase.rooms holds arrays of rooms, one for each floor)

--- Map/MapBase.gd
+++ Map/MapBase.gd
@@ -4,5 +4,5 @@ class_name MapBase
 ##
 ## Holds basic functionality for a map
 
-@export var rooms: Array = [RoomBase] ## 2D array of rooms on the map. this will be assigned when generating rooms
-@export var floors_width: PackedInt32Array = [1, 3, 5, 7, 5, 3, 1] ## array for variable map width, each element is the width of a floor
+@export var rooms: Array[Array] = [] ## 2D array of rooms on the map. this will be assigned when generating rooms
+@export var floors_width: Array[int] = [1, 3, 5, 7, 5, 3, 1] ## array for variable map width, each element is the width of a floor
--- Map/MapManager.gd
+++ Map/MapManager.gd
@@ -1,25 +1,27 @@
 extends Node2D
 ## Class to manage backend for rooms (generation and such)
  
-static func create_map(): ## Generates and Populates a map with rooms that have random room types. More in depth algorithms will be added in the future
+static func create_map() -> MapBase: ## Generates and Populates a map with rooms that have random room types. More in depth algorithms will be added in the future
 	var _map: MapBase = MapBase.new()
-	var _grid: Array = [] ## 2d array to return. this will be populated with rooms
-	
+	var _grid: Array[Array] = [] ## 2d array to return. this will be populated with rooms
+	var _max_floor_size: int = _map.floors_width.max()
 	for index_height: int in range(_map.floors_width.size()): ## loop through the floor and append null to create a square grid
 		_grid.append([])
-		var _max_floor_size: int = _map.floors_width[_map.floors_width.size()/2]
-		var _padding: int = (_max_floor_size - _map.floors_width[index_height])/2
-		var _floor_width: int = _map.floors_width[index_height]
-		for index_padding:int in _padding*2:
-			if _padding > 0:
-				_grid[index_height].insert(index_padding, null)
+		
+		var _floor_width : int = _map.floors_width[index_height]
+		var _padding_size : int = (_max_floor_size - _floor_width)/2
+		
+		_grid[index_height].resize(_max_floor_size-_floor_width)
+		_grid[index_height].fill(null)
 		
 		for index_width: int in range(_map.floors_width[index_height]):## loop through elements of the grid and assign a room type
 			var _rand_type_index: int = randi() % GlobalVar.EVENTS_CLASSIFICATION.size()
 			var _room_event: EventBase = GlobalVar.EVENTS_CLASSIFICATION[_rand_type_index].new()
 			var _generated_room: RoomBase = RoomBase.new()
+			
 			_generated_room.room_event = _room_event
-			_grid[index_height].insert(index_width+_padding,_generated_room)
+			
+			_grid[index_height].insert(index_width+_padding_size,_generated_room as RoomBase)
 	#print(_grid)
 	_map.rooms = _grid
-	return _map
+	return _map as MapBase
