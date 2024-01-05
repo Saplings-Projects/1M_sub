32d5745534b71995a0eb069887a2c43d3a8bf6fc
Enmanuel <holacasa898@gmail.com>
2024-01-05 11:45:14 -0500

    Fixed a function, fixed the test?, and cards not appearing in the UI.

--- Tests/test_pile_ui.gd
+++ Tests/test_pile_ui.gd
@@ -9,31 +9,35 @@ var _card_scroll: Control = null
 const SCROLL_CONTAINER_INDEX: int = 2
 const GRID_CONTAINER_INDEX: int = 0
 
-
-func before_each():
+func before_all():
 	_card_container = _card_container_scene.instantiate()
-	_card_scroll = _card_container_scroll_scene.instantiate()
-	
 	# fill deck with 50 default cards
 	_card_container.default_deck.resize(50)
 	_card_container.max_hand_size = 10
 	_card_container.starting_hand_size = 0
 	_card_container.card_draw_time = .05
-	
+
+func before_each():
+
 	# assign cards names of Card1-Card50
 	for card_index: int in _card_container.default_deck.size():
 		_card_container.default_deck[card_index] = CardBase.new()
 		_card_container.default_deck[card_index].card_title = "Card" + str(card_index + 1)
 	
 	get_tree().root.add_child(_card_container)
-
-	_card_scroll.deck_pile = _card_container.default_deck.duplicate()
+	
 
 func after_each():
-	queue_free()
-
+	
+	
+	_card_container.free()
+	_card_scroll.free()
+	
 func test_populate():
 	
+	_card_scroll = _card_container_scroll_scene.instantiate()
+	_card_scroll.deck_pile = _card_container.default_deck.duplicate()
+
 	_card_scroll.populate("DeckPile")
 
 	var card_pile = _card_scroll.deck_pile
