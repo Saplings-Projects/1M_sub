0504cefe479ec62dacb6a582c1fda1501748eeff
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-01-09 21:07:35 +0100

    Modified EffectDraw to take status into account

--- Cards/Effects/EffectDraw.gd
+++ Cards/Effects/EffectDraw.gd
@@ -3,4 +3,5 @@ class_name EffectDraw extends EffectBase
 # @Override
 @warning_ignore("unused_parameter")
 func apply_effect(caster: Entity, target: Entity, value: int) -> void:
-    CardManager.card_container.draw_cards(value)
+    var modified_value: int = calculate_value("draw", caster, target, value)
+    CardManager.card_container.draw_cards(modified_value)
