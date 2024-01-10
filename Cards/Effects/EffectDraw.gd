229e59605322f5019676c073afed17f3d233407d
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-01-10 12:24:36 +0100

    Remove unneeded warning

--- Cards/Effects/EffectDraw.gd
+++ Cards/Effects/EffectDraw.gd
@@ -1,7 +1,6 @@
 class_name EffectDraw extends EffectBase
 
 # @Override
-@warning_ignore("unused_parameter")
 func apply_effect(caster: Entity, target: Entity, value: int) -> void:
     var modified_value: int = calculate_value("draw", caster, target, value)
     CardManager.card_container.draw_cards(modified_value)
