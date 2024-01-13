5fd27da52ab1000f867ed21683e232a7be1e0ffe
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-01-13 01:58:05 +0100

    Reworked StatModifiers to be able to update constructor default value via export

--- Entity/Components/Stats/StatModifiers.gd
+++ Entity/Components/Stats/StatModifiers.gd
@@ -2,13 +2,23 @@ class_name StatModifiers extends Resource
 
 var modifiers: Dictionary = {}
 
-func _init() -> void:
-    modifiers = {   
-                    "permanent_add":        0,  # int
-                    "permanent_multiply":   1,  # float
-                    "temporary_add":        0,  # int
-                    "temporary_multiply":   1   # float
-                }
+@export var permanent_add: int = 0
+@export var permanent_multiply: float = 1
+@export var temporary_add: int = 0
+@export var temporary_multiply: float = 1 
+
+func _init( _permanent_add: int = permanent_add, 
+			_permanent_multiply: float = permanent_multiply, 
+			_temporary_add: int = temporary_add, 
+			_temporary_multiply: float = temporary_multiply) -> void:
+	modifiers = {   
+					"permanent_add":        _permanent_add,  # int
+					"permanent_multiply":   _permanent_multiply,  # float
+					"temporary_add":        _temporary_add,  # int
+					"temporary_multiply":   _temporary_multiply   # float
+				}
+# the parameter call is a bit convoluted, but this allows to both modify via export
+# and via the constructor when calling new() in code
 
 func reset_temp_to_default() -> void:
     modifiers["temporary_add"] = 0
