6afe68456bc36636a437714a93ec1ef0b24c08f3
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-01-13 01:58:45 +0100

    Solved bug in StatModifiers where the Dictionary was not updated correctly

--- Entity/Components/Stats/StatModifiers.gd
+++ Entity/Components/Stats/StatModifiers.gd
@@ -25,8 +25,8 @@ func reset_temp_to_default() -> void:
     modifiers["temporary_multiply"] = 1
 
 func change_modifier(new_modification: StatModifiers) -> void:
-    modifiers["permanent_add"] += new_modification["permanent_add"]
-    modifiers["permanent_multiply"] *= new_modification["permanent_multiply"]
-    modifiers["temporary_add"] += new_modification["temporary_add"]
-    modifiers["temporary_multiply"] *= new_modification["temporary_multiply"]
+	modifiers["permanent_add"] += new_modification.modifiers["permanent_add"]
+	modifiers["permanent_multiply"] *= new_modification.modifiers["permanent_multiply"]
+	modifiers["temporary_add"] += new_modification.modifiers["temporary_add"]
+	modifiers["temporary_multiply"] *= new_modification.modifiers["temporary_multiply"]
 
