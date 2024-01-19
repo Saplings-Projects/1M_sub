9a0c22751183261759dd58f9d21853f5f6993f2a
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-01-19 23:36:56 +0100

    Remove unneeded (?) ignore_caster/target_status in Debuff_Poison

--- Status/Debuffs/Debuff_Poison.gd
+++ Status/Debuffs/Debuff_Poison.gd
@@ -11,7 +11,5 @@ func on_turn_start() -> void:
 	var damage_data: DealDamageData = DealDamageData.new()
 	damage_data.damage = status_power
 	damage_data.caster = status_caster
-	damage_data.ignore_caster_status = true
-	damage_data.ignore_target_status = true
 	
 	status_target.get_health_component().deal_damage(damage_data)
