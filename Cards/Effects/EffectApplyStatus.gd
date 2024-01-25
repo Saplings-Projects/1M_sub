81e1d6e93cba1ff3e06783b290ce2babd13b0f49
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-01-25 15:14:42 +0100

    Create a new effect to apply a status
    This allows us to not have to create a new effect every time we create a new status

---
+++ Cards/Effects/EffectApplyStatus.gd
@@ -0,0 +1,8 @@
+class_name EffectApplyStatus extends EffectBase
+
+@export var status_to_apply: StatusBase
+
+# @Override
+@warning_ignore("unused_parameter")
+func apply_effect(caster: Entity, target: Entity, value: int) -> void:
+    target.get_status_component().add_status(status_to_apply, caster)
