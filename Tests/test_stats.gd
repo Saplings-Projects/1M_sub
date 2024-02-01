fedc73f9348121555d658184083f7152501b331a
tynutsathitya <tynutsathitya@gmail.com>
2024-01-31 23:58:59 -0800

    Issue-51: Test orphan assert error on test_stats

--- Tests/test_stats.gd
+++ Tests/test_stats.gd
@@ -43,9 +43,9 @@ func before_each():
 
 func after_each():
 	_player.queue_free()
-	_enemy.queue_free()
-	_enemy_2.queue_free()
-	_battler.queue_free()
+	#_enemy.queue_free()
+	#_enemy_2.queue_free()
+	#_battler.queue_free()
 	#_card_container.queue_free()
 	assert_no_new_orphans("Orphans still exist, please free up test resources.")
 
