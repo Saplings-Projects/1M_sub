35069271912e765b1dcf1e9a6561bcdd9fcce114
Tomzkk <Tomzkk@users.noreply.github.com>
2024-02-05 18:42:52 +0100

    Added tests

--- Tests/test_death_handling.gd
+++ Tests/test_death_handling.gd
@@ -45,6 +45,7 @@ func before_each():
 	
 	# disconnecting signal as _combat_end_change_scene causes scene to be (re)loaded 
 	# which if called from test actually starts the game and the test doesnt end
+	watch_signals(PhaseManager)
 	if PhaseManager.is_connected("on_combat_end", SceneController._combat_end_change_scene):
 		PhaseManager.disconnect("on_combat_end", SceneController._combat_end_change_scene)
 
@@ -58,18 +59,25 @@ func after_each():
 	_free_if_valid(_enemy)
 	_free_if_valid(_enemy_2)
 	_battler.queue_free()
+	
+
+func test_player_death_during_enemy_turn():
+	_player.get_health_component()._set_health(1.0)
+	_battler._on_enemy_start_turn()
+	assert_eq(_player.get_health_component().current_health, 0.)
+	assert_signal_emitted_with_parameters(PhaseManager, "on_combat_end", [Enums.CombatResult.DEFEAT])
+
 
 func test_check_and_handle_battle_end_player_death():
 	_player_health_component._set_health(0.)
-	watch_signals(PhaseManager)
 		
 	_battler._check_and_handle_battle_end()
 
 	assert_signal_emitted_with_parameters(PhaseManager, "on_combat_end", [Enums.CombatResult.DEFEAT])
 	
+
 func test_check_and_handle_battle_end_enemy_death():
 	_battler._enemy_list = []
-	watch_signals(PhaseManager)
 	
 	_battler._check_and_handle_battle_end()
 
@@ -108,6 +116,16 @@ func test_enemy_death_to_player_attack():
 	assert_eq(_enemy_list.size(), 1)
 
 
+func test_all_enemy_death_to_player_attack_all():
+	_enemy_health_component._set_health(1.0)
+	_enemy_2_health_component._set_health(1.0)
+	var card_damage_all: CardBase = load("res://Cards/Resource/Card_DamageAll.tres")
+	
+	assert_eq(_enemy_list.size(), 2)
+	card_damage_all.on_card_play(_player, [])
+	assert_eq(_enemy_list.size(), 0)
+
+
 func test_enemy_death_to_poison():
 	_enemy_health_component._set_health(1.0)
 	var card_poison: CardBase = load("res://Cards/Resource/Card_Poison.tres")
@@ -115,4 +133,17 @@ func test_enemy_death_to_poison():
 
 	assert_eq(_enemy_list.size(), 2)
 	_battler._on_enemy_start_turn()
+	assert_eq(_enemy_list.size(), 1)
+
+
+func test_enemy_death_to_expiring_poison():
+	_enemy_health_component._set_health(1.0)
+	var card_poison: CardBase = load("res://Cards/Resource/Card_Poison.tres")
+	card_poison.on_card_play(_player, [_enemy])
+	_enemy.get_status_component().current_status[0].status_turn_duration = 1
+
+	assert_eq(_enemy_list.size(), 2)
+	assert_eq(_enemy.get_status_component().current_status.size(), 1)
+	_battler._on_enemy_start_turn()
+	assert_eq(_enemy.get_status_component().current_status.size(), 0)
 	assert_eq(_enemy_list.size(), 1)
