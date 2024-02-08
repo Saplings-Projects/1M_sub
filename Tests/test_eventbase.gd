e00a3127fa4da021533668f2ff4eb446cbd8a077
Tyler Nutsathitya <tynutsathitya@gmail.com>
2024-02-07 20:14:05 -0800

    Issue-51: Update GUT Tests to assert if there are no orphans (#52)
    * Issue-51: Check for Orphans WIP
    
    * Issue-51: Test malformed formatting
    
    * Issue-51: Test assert-check on CI
    
    * Issue-51: Test modifying config to enable errors on compile
    
    * Issue-51: Revert Changes on config, test my forked action
    
    * Issue-51: Fix formatting
    
    * Issue-51: Add assertion for no orphans in test cases
    
    * Issue-51: Comment out using config
    
    * Issue-51: Test orphan error assert on test_stats
    
    * Issue-51: Test orphan assert error on test_stats
    
    * Issue-51: Uncomment lines to clean up tests

--- Tests/test_eventbase.gd
+++ Tests/test_eventbase.gd
@@ -6,6 +6,9 @@ var eventBase: EventBase = null
 func before_each():
 	eventBase = EventBase.new()
 
+func after_each():
+	assert_no_new_orphans("Orphans still exist, please free up test resources.")
+
 func test_event_base_array_all_types():
 	var event0 = eventBase.EVENTS_CLASSIFICATION[0]
 	assert_eq(event0, EventMob)
