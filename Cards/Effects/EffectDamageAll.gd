1dd4275017a5f4339f1149c05ba0ce1425b84ad6
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-02-11 21:02:46 +0100

    Remove EffectDamageAll as it can be made from an EffectDamage and a TargetAllEnemies

--- Cards/Effects/EffectDamageAll.gd
+++
@@ -1,9 +0,0 @@
-class_name EffectDamageAll extends EffectBase
-
-func apply_effect(caster: Entity, target: Entity, value: int) -> void:
-	var target_damage_data: DealDamageData = DealDamageData.new()
-	var party = target.get_party_component().party
-	target_damage_data.damage = value
-	target_damage_data.caster = caster
-	for party_target in party:
-		party_target.get_health_component().deal_damage(target_damage_data)
