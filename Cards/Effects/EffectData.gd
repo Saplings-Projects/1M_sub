3781314b8e6f0044c070b98e92f7143649c09faa
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-02-11 22:45:57 +0100

    Remove list_targets in EffectData as it is not needed anymore

--- Cards/Effects/EffectData.gd
+++ Cards/Effects/EffectData.gd
@@ -4,21 +4,15 @@ class_name EffectData extends Resource
 
 @export var effect: EffectBase = null
 var caster: Entity = null
-var list_targets: Array[Entity] = []
 @export var value: int = 0
-# @export var target_type : Enums.TargetType = Enums.TargetType.ALL_TARGETS #TODO Target change
-# ? still needed given targeting function
-# ? or can remove since the targeting function includes the target_type
 @export var targeting_function: TargetingBase = null
 
 func _init(	_effect: EffectBase = null,
 			_caster: Entity = null, 
-			_list_targets: Array[Entity] = [], 
 			_value: int = 0, 
 			_targeting_function: TargetingBase = null) -> void:
 	self.effect = _effect
 	self.caster = _caster
-	self.list_targets = _list_targets
 	self.value = _value
 	self.targeting_function = _targeting_function
 
