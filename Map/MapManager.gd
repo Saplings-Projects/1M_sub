90d0e6e14b011dfebddecdaadecd57efdf252799
tynutsathitya <tynutsathitya@gmail.com>
2024-02-11 12:50:53 -0800

    Issue-46: Remove static function from create_map, Update Unit Test

--- Map/MapManager.gd
+++ Map/MapManager.gd
@@ -5,7 +5,7 @@ var current_map: MapBase
 var map_width_array: Array[int] = [1, 3, 5, 7, 9, 11, 9, 7, 5, 3, 1]
  
 #map_floors_width changes the width of the map's floors
-static func create_map(map_floors_width: Array[int]) -> MapBase: ## Generates and Populates a map with rooms that have random room types. More in depth algorithms will be added in the future
+func create_map(map_floors_width: Array[int]) -> MapBase: ## Generates and Populates a map with rooms that have random room types. More in depth algorithms will be added in the future
 	var _map: MapBase = MapBase.new()
 	var _grid: Array[Array] = [] ## 2d array to return. this will be populated with rooms
 	var _max_floor_size: int = map_floors_width.max()
@@ -31,7 +31,7 @@ static func create_map(map_floors_width: Array[int]) -> MapBase: ## Generates an
 	return _map as MapBase
 	
 func _ready():
-	current_map = MapManager.create_map(map_width_array)
+	current_map = create_map(map_width_array)
 	
 func is_map_initialized() -> bool:
 	return current_map != null
