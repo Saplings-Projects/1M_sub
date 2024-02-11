4b3f58f612523804379d991cc357530fec749d60
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-02-11 02:38:09 +0100

    Make a common setup function for all tests (#63)
    * Add TestBase for common setup between health/stats
    
    * Add TestCardBase for common setup on decks/card related tests

--- Tests/test_cards.gd
+++ Tests/test_cards.gd
@@ -1,35 +1,14 @@
-extends GutTest
+extends TestCardBase
 ## Tests for cards
 
 
-var _card_container_scene: PackedScene = load("res://Cards/CardContainer.tscn")
-var _card_container: CardContainer = null
-
-
+# @Override
 func before_each():
-	_card_container = _card_container_scene.instantiate()
-	
-	# fill deck with 50 default cards
-	_card_container.default_deck.resize(50)
-	_card_container.max_hand_size = 10
-	_card_container.starting_hand_size = 0
-	_card_container.card_draw_time = .05
-	
-	# assign cards names of Card1-Card50
-	for card_index: int in _card_container.default_deck.size():
-		_card_container.default_deck[card_index] = CardBase.new()
-		_card_container.default_deck[card_index].card_title = "Card" + str(card_index + 1)
-	
-	get_tree().root.add_child(_card_container)
+	super()
 	
 	# set draw pile to default deck. This removes any randomness from shuffling on _ready()
 	# so each card will have a name Card1-Card50 in the draw pile
 	_card_container.draw_pile = _card_container.default_deck.duplicate()
-
-
-func after_each():
-	_card_container.queue_free()
-	assert_no_new_orphans("Orphans still exist, please free up test resources.")
 
 
 func test_draw_cards():
