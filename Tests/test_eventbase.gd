99dca4be2ea5885f472b071b20686cd9c2cad7eb
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-02-13 23:17:59 +0100

    Change typing for test_eventbase

--- Tests/test_eventbase.gd
+++ Tests/test_eventbase.gd
@@ -10,15 +10,15 @@ func after_each() -> void:
 	assert_no_new_orphans("Orphans still exist, please free up test resources.")
 
 func test_event_base_array_all_types() -> void:
-	var event0: EventBase = eventBase.EVENTS_CLASSIFICATION[0]
+	var event0: Resource = eventBase.EVENTS_CLASSIFICATION[0]
 	assert_eq(event0, EventMob)
 	
-	var event1: EventBase = eventBase.EVENTS_CLASSIFICATION[1]
+	var event1: Resource = eventBase.EVENTS_CLASSIFICATION[1]
 	assert_eq(event1, EventRandom)
 	
-	var event2: EventBase = eventBase.EVENTS_CLASSIFICATION[2]
+	var event2: Resource = eventBase.EVENTS_CLASSIFICATION[2]
 	assert_eq(event2, EventShop)
 	
-	var event3: EventBase = eventBase.EVENTS_CLASSIFICATION[3]
+	var event3: Resource = eventBase.EVENTS_CLASSIFICATION[3]
 	assert_eq(event3, EventHeal)
 
