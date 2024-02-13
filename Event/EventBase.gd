f50a7ee4bb4eb5b93277297ddf875bb8b9032889
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-02-13 23:11:13 +0100

    Add comment for a place that might need a rework

--- Event/EventBase.gd
+++ Event/EventBase.gd
@@ -2,6 +2,8 @@ extends Resource
 class_name EventBase
 
 var EVENTS_CLASSIFICATION: Array[Resource] = [EventMob, EventRandom, EventShop, EventHeal]
+# ! this should be done by an enum, because having an array of classes is not gonna work nicely
+# plus it should probably be EventMob.new() instead of EventMob
 
 var packedScene: String = ""
 
