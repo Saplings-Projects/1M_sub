213acb315b8d944ef9b11148c98ca26f2c91d4bc
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-02-15 23:03:51 +0100

    Use Vector2i instead of Vector2 for movements to remove type casting

--- Global/GLOBAL_VAR.gd
+++ Global/GLOBAL_VAR.gd
@@ -3,7 +3,7 @@ extends Node## singleton used to store global values
 var EVENTS_CLASSIFICATION: Array[Resource] = [EventMob, EventRandom, EventShop, EventHeal]
 
 var POSSIBLE_MOVEMENTS: Dictionary = {
-	"UP": Vector2(0,1),
-	"LEFT": Vector2(-1,1),
-	"RIGHT": Vector2(1,1)
+	"UP": Vector2i(0,1),
+	"LEFT": Vector2i(-1,1),
+	"RIGHT": Vector2i(1,1)
 }
--- Map/MapMovement.gd
+++ Map/MapMovement.gd
@@ -1,12 +1,12 @@
 class_name MapMovement extends Resource
 
 
-static func get_all_accessible_rooms(floor_index: int, room_index: int) -> Array[RoomBase]:
+static func get_accessible_rooms_by_player(floor_index: int, room_index: int) -> Array[RoomBase]:
 	var accessible_rooms: Array[RoomBase] = []
 	var map_width_array: Array[int] = MapManager.map_width_array
-	for movement: Vector2 in GlobalVar.POSSIBLE_MOVEMENTS:
-		var new_floor_index: int = floor_index + (movement.y as int)
-		var new_room_index: int = room_index + (movement.x as int)
+	for movement: Vector2i in GlobalVar.POSSIBLE_MOVEMENTS:
+		var new_floor_index: int = floor_index + movement.y
+		var new_room_index: int = room_index + movement.x
 		if new_floor_index >= 0 and new_floor_index <= map_width_array.size() - 1:
 			if new_room_index >= 0 and new_room_index <= map_width_array[new_floor_index] - 1:
 				var room: RoomBase = MapManager.current_map.rooms[new_floor_index][new_room_index]
@@ -14,3 +14,4 @@ static func get_all_accessible_rooms(floor_index: int, room_index: int) -> Array
 					accessible_rooms.append(room)
 	
 	return accessible_rooms
+	
