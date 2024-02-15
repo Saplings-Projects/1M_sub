0fa1d69d3be83f31c2645ad1506f287d703e23cb
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-02-16 00:02:52 +0100

    Change player_position to not be set in the set_player

--- Managers/PlayerManager.gd
+++ Managers/PlayerManager.gd
@@ -4,15 +4,18 @@ extends Node
 ## Since we only have one player, you can easily get the player from anywhere by calling
 ## PlayerManager.player.
 
+signal on_player_initialized
 
 var player: Player
 var player_position: Vector2i
 
-signal on_player_initialized
+
+func _ready() -> void:
+	player = null
+	player_position = Vector2i(0, 0)
 
 
-func set_player(in_player: Player, in_player_position: Vector2i) -> void:
+func set_player(in_player: Player) -> void:
 	player = in_player
-	player_position = in_player_position
 	if player != null:
 		on_player_initialized.emit()
