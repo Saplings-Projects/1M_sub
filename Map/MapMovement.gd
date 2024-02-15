c4fda3af65d3342d610c57d1a383b7efaea95fb0
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-02-15 23:43:49 +0100

    Accessible rooms by player now returns a list of positions
    This allows it to be used as a general description whatever the of rooms in the actual map is (RoomBase, RoomUI...)

--- Map/MapMovement.gd
+++ Map/MapMovement.gd
@@ -1,17 +1,18 @@
 class_name MapMovement extends Resource
 
 
-static func get_accessible_rooms_by_player(floor_index: int, room_index: int) -> Array[RoomBase]:
-	var accessible_rooms: Array[RoomBase] = []
+static func get_accessible_room_positions_by_player(position: Vector2i) -> Array[Vector2i]:
+	var accessible_room_positions: Array[Vector2i] = []
 	var map_width_array: Array[int] = MapManager.map_width_array
 	for movement: Vector2i in GlobalVar.POSSIBLE_MOVEMENTS:
-		var new_floor_index: int = floor_index + movement.y
-		var new_room_index: int = room_index + movement.x
+		var new_floor_index: int = position.y + movement.y
+		var new_room_index: int = position.x + movement.x
 		if new_floor_index >= 0 and new_floor_index <= map_width_array.size() - 1:
 			if new_room_index >= 0 and new_room_index <= map_width_array[new_floor_index] - 1:
 				var room: RoomBase = MapManager.current_map.rooms[new_floor_index][new_room_index]
+				# assuming all maps whatever the type are based on the same width array
 				if room != null:
-					accessible_rooms.append(room)
+					accessible_room_positions.append(Vector2i(new_room_index, new_floor_index))
 	
-	return accessible_rooms
+	return accessible_room_positions
 	
