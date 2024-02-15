2227804542e66886a232ee1bce31db686f22a1ef
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-02-15 23:55:27 +0100

    Add player position as var of PlayerManager

--- Managers/PlayerManager.gd
+++ Managers/PlayerManager.gd
@@ -6,11 +6,13 @@ extends Node
 
 
 var player: Player
+var player_position: Vector2i
 
 signal on_player_initialized
 
 
-func set_player(in_player: Player) -> void:
+func set_player(in_player: Player, in_player_position: Vector2i) -> void:
 	player = in_player
+	player_position = in_player_position
 	if player != null:
 		on_player_initialized.emit()
