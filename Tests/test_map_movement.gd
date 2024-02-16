65ebdbd8f88f4af17853ff49c543f8d1d08e6d77
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-02-16 03:03:47 +0100

    Modify map movement test to be able to test multiple starting positions

--- Tests/test_map_movement.gd
+++ Tests/test_map_movement.gd
@@ -1,8 +1,14 @@
 extends TestMapBase
 
 func test_accessible_positions_by_player() -> void:
-	var current_player_position: Vector2i = Vector2i(2, 4) # the room at the bottom of the map
-	var accessible_positions: Array[Vector2i] = MapMovement.get_accessible_room_positions_by_player(current_player_position)
-	assert_has(accessible_positions, Vector2i(1,3))
-	assert_has(accessible_positions, Vector2i(2,3))
-	assert_has(accessible_positions, Vector2i(3,3))
+	var player_position_array: Array[Vector2i] = [Vector2i(2,4), Vector2i(4,2)]
+	var expected_accessible_positions: Array[Array] = [
+		[Vector2i(1,3), Vector2i(2,3), Vector2i(3,3)],
+		[Vector2i(3,1)]
+	]
+	for i: int in range(player_position_array.size()):
+		var current_player_position: Vector2i = player_position_array[i]
+		assert_not_null(MapManager.current_map.rooms[current_player_position.y][current_player_position.x])
+		var accessible_positions: Array[Vector2i]  = MapMovement.get_accessible_room_positions_by_player(current_player_position)
+		for position: Vector2i in expected_accessible_positions[i]:
+			assert_has(accessible_positions,position)
