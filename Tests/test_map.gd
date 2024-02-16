d02ccca05a000cb40fcda9158cc3482724becf03
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-02-16 02:47:42 +0100

    Add TestMapBase as parent of all tests on map

--- Tests/test_map.gd
+++ Tests/test_map.gd
@@ -1,21 +1,13 @@
-extends GutTest ## Tests for MapManager, more will be added in the future
+extends TestMapBase ## Tests for MapManager, more will be added in the future
 
-var test_generator = load("res://Map/MapManager.gd")
-var map_manager: MapManager = null
 
-func test_map_gen():
-	map_manager = test_generator.new()
-	var test_width: Array[int] = [1,3,5,3,1]
-	var test_map: MapBase = map_manager.create_map(test_width)
+func test_map_gen() -> void:
 	var expected_null_array: Array[Array] = [[0,0], [0,1], [0,3],[0,4],[1,0],[1,4],[3,0],[3,4],[4,0],[4,1],[4,3],[4,4]]
 	var expected_exists_array: Array[Array] = [[0,2],[1,1],[1,2],[1,3],[2,0],[2,1],[2,2],[2,3],[2,4],[3,1],[3,2],[3,3],[4,2]]
 	for couple: Array[int] in expected_null_array:
-		var _room = test_map.rooms[couple[0]][couple[1]]
+		var _room: RoomBase = test_map.rooms[couple[0]][couple[1]]
 		assert_null(_room, "Expected null at %s but got %s" % [couple, _room])
 
 	for couple: Array[int] in expected_exists_array:
-		var _room = test_map.rooms[couple[0]][couple[1]]
+		var _room: RoomBase = test_map.rooms[couple[0]][couple[1]]
 		assert_not_null(_room, "Expected not null at %s but got %s" % [couple, _room])
-
-func after_each():
-	map_manager.free()
