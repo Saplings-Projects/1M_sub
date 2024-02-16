40cf6445c7cc66f60b71cd130b0bfca36315dac4
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-02-16 03:09:58 +0100

    Add more starting position to tests

--- Tests/test_map_movement.gd
+++ Tests/test_map_movement.gd
@@ -1,10 +1,19 @@
 extends TestMapBase
 
 func test_accessible_positions_by_player() -> void:
-	var player_position_array: Array[Vector2i] = [Vector2i(2,4), Vector2i(4,2)]
+	var player_position_array: Array[Vector2i] = [
+		Vector2i(2,4), 
+		Vector2i(4,2),
+		Vector2i(0,2),
+		Vector2i(1,2),
+		Vector2i(2,0)
+	]
 	var expected_accessible_positions: Array[Array] = [
 		[Vector2i(1,3), Vector2i(2,3), Vector2i(3,3)],
-		[Vector2i(3,1)]
+		[Vector2i(3,1)],
+		[Vector2i(1,1)],
+		[Vector2i(1,1), Vector2i(2,1)],
+		[]
 	]
 	for i: int in range(player_position_array.size()):
 		var current_player_position: Vector2i = player_position_array[i]
