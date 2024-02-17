0a7211d06f8e60b967cf5293b9edeea46d43105e
Enmanuel Lizardo <Holacasa898@gmail.com>
2024-02-17 18:15:16 -0500

    Argument no more.

--- UI/EnergyLabel.gd
+++ UI/EnergyLabel.gd
@@ -6,11 +6,11 @@ var energy_component: EnergyComponent
 func _ready() -> void:
 	energy_component = PlayerManager.player.get_energy_component()
 	energy_component.on_energy_changed.connect(on_energy_changed)
-	CardManager.on_card_action_finished.connect(on_card_played)
+	CardManager.on_card_action_finished.connect(on_card_played.unbind(1))
 	update_label()
 
 
-func on_card_played(_card: CardWorld) -> void:
+func on_card_played() -> void:
 	update_label()
 
 func on_energy_changed() -> void:
