862eda76fca1c1a154efe761df232afe1e8f97c3
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-02-18 22:59:12 +0100

    Clear up a misunderstanding regarding map structure

--- Global/GLOBAL_VAR.gd
+++ Global/GLOBAL_VAR.gd
@@ -3,9 +3,7 @@ extends Node## singleton used to store global values
 var EVENTS_CLASSIFICATION: Array[Resource] = [EventMob, EventRandom, EventShop, EventHeal]
 
 var POSSIBLE_MOVEMENTS: Dictionary = {
-	"UP": Vector2i(0,-1),
-	"LEFT": Vector2i(-1,-1),
-	"RIGHT": Vector2i(1,-1)
+	"UP": Vector2i(0,1),
+	"LEFT": Vector2i(-1,1),
+	"RIGHT": Vector2i(1,1)
 }
-# it's a bit counter intuitive, but level 0 is the start of the array which means the top of the map
-# so when we go "UP" in the map, we go towards the index 0 of the array
