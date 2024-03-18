69047f57cc4637e47b1584ec77174793d4483c43
Seth Burchfield <151570126+cheesefrycook@users.noreply.github.com>
2024-03-18 17:30:21 -0500

    fix error in test

--- Tests/test_card_animation.gd
+++ Tests/test_card_animation.gd
@@ -16,30 +16,40 @@ func after_each() -> void:
 
 
 func test_player_animation_created() -> void:
-	var card: CardBase = load("res://Cards/Resource/Card_Damage.tres").duplicate()
+	var card: CardBase = CardBase.new()
 	var cast_anim: CastAnimationData = CastAnimationData.new()
 	cast_anim.cast_animation_scene = load("res://Cards/Animation/Anim_Slap.tscn")
 	cast_anim.cast_position = CastPos_Caster.new()
+	
+	card.application_type = GlobalEnums.ApplicationType.ENEMY_ONLY
+	card.card_effects_data.append(EffectData.new())
 	card.card_effects_data[0].animation_data = cast_anim
+	card.card_effects_data[0].targeting_function = TargetingBase.new()
+	card.card_effects_data[0].effect = EffectDamage.new()
 	
 	card.on_card_play(_player, _enemy)
 	
-	# CastPos_Caster should be a child of player
+	# Animation should have been created as a child of the player
 	created_cast_nodes = Helpers.get_all_children_nodes_of_type(_player, CastAnimation)
 	
 	assert_eq(created_cast_nodes.size(), 1)
 
 
 func test_enemy_animation_created() -> void:
-	var card: CardBase = load("res://Cards/Resource/Card_Damage.tres").duplicate()
+	var card: CardBase = CardBase.new()
 	var cast_anim: CastAnimationData = CastAnimationData.new()
 	cast_anim.cast_animation_scene = load("res://Cards/Animation/Anim_Slap.tscn")
 	cast_anim.cast_position = CastPos_Caster.new()
+	
+	card.application_type = GlobalEnums.ApplicationType.FRIENDLY_ONLY
+	card.card_effects_data.append(EffectData.new())
 	card.card_effects_data[0].animation_data = cast_anim
+	card.card_effects_data[0].targeting_function = TargetingBase.new()
+	card.card_effects_data[0].effect = EffectDamage.new()
 	
 	card.on_card_play(_enemy, _player)
 	
-	# CastPos_Caster should be a child of enemy
+	# Animation should have been created as a child of the enemy
 	created_cast_nodes = Helpers.get_all_children_nodes_of_type(_enemy, CastAnimation)
 	
 	assert_eq(created_cast_nodes.size(), 1)
@@ -47,12 +57,16 @@ func test_enemy_animation_created() -> void:
 
 func test_cast_position_all() -> void:
 	# Create CardBase with CastPos_AllTargets and ensure one is created for each target
-	var card: CardBase = load("res://Cards/Resource/Card_Damage.tres").duplicate()
+	var card: CardBase = CardBase.new()
 	var cast_anim: CastAnimationData = CastAnimationData.new()
 	cast_anim.cast_animation_scene = load("res://Cards/Animation/Anim_Slap.tscn")
 	cast_anim.cast_position = CastPos_AllTargets.new()
+	
+	card.application_type = GlobalEnums.ApplicationType.ENEMY_ONLY
+	card.card_effects_data.append(EffectData.new())
 	card.card_effects_data[0].animation_data = cast_anim
 	card.card_effects_data[0].targeting_function = TargetAllEnemies.new()
+	card.card_effects_data[0].effect = EffectDamage.new()
 	
 	card.on_card_play(_player, _enemy)
 	
