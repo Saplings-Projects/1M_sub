dab628344482aebc2864c25e1cd5bcba16fb2471
Jona Lam <89871543+JonaLam@users.noreply.github.com>
2024-03-24 21:45:37 +0100

    Changed function for better scaleability

--- addons/SmoothScroll/SmoothScrollContainer.gd
+++ addons/SmoothScroll/SmoothScrollContainer.gd
@@ -634,11 +634,16 @@ func scroll_to_fauna(duration:float=0.5) -> void:
 	if(!PlayerManager.is_player_initial_position_set):
 		return
 	
-	var room_distance: float = 66
-	var scroll_to_pos:float = -room_distance
-	var rooms_between_fauna_and_top : int = MapManager.map_width_array.size() - 8
+	var map_UI : MapUI = get_parent()
+	var current_map_texure_button : TextureButton = Helpers.get_first_child_node_of_type(map_UI.current_player_room, TextureButton)
 	
-	scroll_to_pos -= (rooms_between_fauna_and_top - PlayerManager.player_position.y) * room_distance 
+	var max_scroll = content_node.size.y - self.size.y
+	var room_distance : float = map_UI.get_combined_room_height(current_map_texure_button) / MapManager.current_map.rooms.size()
+	var scroll_to_pos : float = -max_scroll
+	
+	var rooms_behind_seen : int = 1
+	
+	scroll_to_pos += room_distance * (PlayerManager.player_position.y - rooms_behind_seen)
 	
 	scroll_y_to(scroll_to_pos)
 
