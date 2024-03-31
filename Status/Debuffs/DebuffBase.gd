0a1ebfc91b1234483a984392a084cf3c571a4344
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-03-31 19:41:15 +0200

    Comment the status

--- Status/Debuffs/DebuffBase.gd
+++ Status/Debuffs/DebuffBase.gd
@@ -1,7 +1,7 @@
 extends StatusBase
 class_name DebuffBase
-
 ## This class doesn't do much for now except for being a container for all the debuff
+##
 ## This is useful to directly know if a certain class is a debuff given its parent
-## which was not the case when we just had StatusBase
+## which was not the case when we just had StatusBase [br]
 ## This can be used later to have different visuals or different objects being modified by buffs or debuffs
