71494bbdcebdffa76f736fa7fbfd192b022a8c77
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-03-31 14:36:53 +0200

    Comment all entity code

--- Entity/Components/HealthComponent.gd
+++ Entity/Components/HealthComponent.gd
@@ -1,14 +1,20 @@
 extends EntityComponent
 class_name HealthComponent
-## Holds player health values and allows for dealing damage.
+## Everything related to health for all entities
+##
+## Holds player / enemies health values and allows for dealing damage.
 
-
+## Emit when health is changed
 signal on_health_changed(new_health: int)
 
+## The maximum health the entity can have; this is a hard limit
 @export var max_health: float = 100
-var current_health: float = 100
+## The health the entity currently has [br]
+## Between 0 and max_health
+var current_health: float = 100 # ? change this to max health, or just unset since it's init by the ready
 
-
+## Initialize the health component, putting health to max health
+## This only happens at game start, because this is globally loaded
 func _ready() -> void:
 	_set_health(max_health)
 
@@ -16,7 +22,9 @@ func _ready() -> void:
 # Allow caster to be null, but not the target.
 # If caster is null, we assume that the damage came from an unknown source,
 # so status won't calculate.
-# Use a negative damage value if you want to heal.
+## Deal damage to the entity [br]
+## Use a negative damage value if you want to heal. [br]
+## Caster can be null [br]
 func deal_damage(damage: float, caster: Entity) -> void:
 	
 	if damage == 0.0:
@@ -24,7 +32,7 @@ func deal_damage(damage: float, caster: Entity) -> void:
 	
 	assert(owner != null, "No owner was set. Please call init on the Entity.")
 	
-	var target: Entity = entity_owner
+	var target: Entity = entity_owner # TODO change this name entity_owner to make it clearer
 	
 	# if this was a self attack, ignore the caster
 	if caster == target: 
@@ -35,6 +43,7 @@ func deal_damage(damage: float, caster: Entity) -> void:
 	_set_health(new_health)
 
 
+## Set the health of the entity [br]
 func _set_health(new_health: float) -> void:
 	if (new_health == current_health):
 		return
