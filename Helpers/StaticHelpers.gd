ebd329dbe2a9fa7d67caf63956cbf78c21370c5f
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-04-02 23:33:59 +0200

    Comment helpers

--- Helpers/StaticHelpers.gd
+++ Helpers/StaticHelpers.gd
@@ -3,6 +3,7 @@ class_name Helpers
 ## A place for helpful functions we can use globally.
 
 
+## Gets the first child node of a given node by it's type
 static func get_first_child_node_of_type(node: Node, type: Variant) -> Variant:
 	for child: Variant in node.get_children():
 		if is_instance_of(child, type):
@@ -10,6 +11,7 @@ static func get_first_child_node_of_type(node: Node, type: Variant) -> Variant:
 	return null
 
 
+## Gets all children nodes of a given node by it's type
 static func get_all_children_nodes_of_type(node: Node, type: Variant) -> Array[Variant]:
 	var children: Array[Variant] = []
 	for child: Variant in node.get_children():
@@ -18,6 +20,7 @@ static func get_all_children_nodes_of_type(node: Node, type: Variant) -> Array[V
 	return children
 
 
+## Find the first element of a given type in an array
 static func find_first_from_array_by_type(array: Array[Variant], type: Variant) -> Variant:
 	for value: Variant in array:
 		if is_instance_of(value, type):
@@ -25,10 +28,12 @@ static func find_first_from_array_by_type(array: Array[Variant], type: Variant) 
 	return null
 
 
+## Get a index between 0 and the size of the array -1
 static func get_random_array_index(array: Array[Variant]) -> int:
 	return randi_range(0, array.size() - 1)
 
 
+## Linear mapping of a range [a,b] to range [c,d] (with a mapping to c and b mapping to d)
 static func convert_from_range(value: float, from_min: float, from_max: float, to_min: float, to_max: float) -> float:
 	var from_range: float = from_max - from_min
 	var to_value: float = 0.0
@@ -42,6 +47,7 @@ static func convert_from_range(value: float, from_min: float, from_max: float, t
 	return to_value
 
 
+## Generate the average vector of a list of vectors
 static func get_mean_vector(positions: Array[Vector2]) -> Vector2:
 	if positions.size() <= 0:
 		return Vector2.ZERO
@@ -50,7 +56,10 @@ static func get_mean_vector(positions: Array[Vector2]) -> Vector2:
 		mean_vector += position
 	return mean_vector / positions.size()
 
-# ! works in place
+
+
+## Remove duplicates from an array [br]
+## ! works in place
 static func remove_duplicate_in_array(array: Array) -> void:
 	var dict: Dictionary = {}
 	for value: Variant in array:
