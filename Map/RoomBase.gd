96a12825c369699fa5b527861f6500fb1e37757c
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-04-02 23:46:44 +0200

    Comment Map (for what I'm sure I know at least)

--- Map/RoomBase.gd
+++ Map/RoomBase.gd
@@ -4,19 +4,25 @@ class_name RoomBase
 ##
 ## Holds basic functionality for a room on the map
 
+## Information about the level of light in a room
 @export var light_data: LightData
+## The event in the room (see [EventBase])
 @export var room_event: EventBase
+## The position of the room on the map [br]
+## The first floor is at index 0 [br]
+## Rooms are indexed by their distance to the map border, so the first room of the first floor could be in position [2,0] [br]
+## See [MapMovement] for more information [br]
 var room_position: Vector2i
 
+## Init the light data
 func _init() -> void:
 	light_data = LightData.new()
 
-func _to_string() -> String:
-	return "RoomBase"
-
+## Wrapper around [method RoomEvent.get_room_abbreviation]
 func get_room_abbreviation() -> String:
 	return room_event.get_room_abbreviation()
 
+## Put a torch in the given room and light up the rooms that are in the range of the torch
 func set_torch_active() -> void:
 	light_data.has_torch = true
 	light_data.increase_light_by_torch()
