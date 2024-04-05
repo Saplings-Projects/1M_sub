ef3055606798fc9e8daa5b4611e77e09bdc549a5
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-04-05 20:07:42 +0200

    Comment the TestBase files

--- Tests/TestCardBase.gd
+++ Tests/TestCardBase.gd
@@ -1,13 +1,19 @@
 class_name TestCardBase extends GutTest
 ## Common test setup for CardManager related tests
 
+## The scene for the card container
 var _card_container_scene: PackedScene = load("res://Cards/CardContainer.tscn")
+## The card container instance
 var _card_container: CardContainer = null
 
-
+## ? What does this do
 const SCROLL_CONTAINER_INDEX: int = 2
+## ? What does this do
 const GRID_CONTAINER_INDEX: int = 0
 
+## Setup the environment before each test [br]
+## To be overidden by child tests if they want to do something more or something else [br]
+## Child tests that just want to add some stuff can call super to do the initial setup, then their own setup
 func before_each() -> void:
 	_card_container = _card_container_scene.instantiate()
 	
@@ -28,4 +34,6 @@ func before_each() -> void:
 func after_each() -> void:
 	_card_container.queue_free()
 	CardManager.current_deck.clear()
+	# Check that everything has been set free properly [br]
+	# This is mainly useful for the CI as some tests can fail silently and the only indication is that resources are not freed
 	assert_no_new_orphans("Orphans still exist, please free up test resources.")
