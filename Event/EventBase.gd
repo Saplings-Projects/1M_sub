71fda48b1cc29ce487d81e3971045c0b69428009
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-04-10 20:38:06 +0200

    Add on_event_started/ended functions

--- Event/EventBase.gd
+++ Event/EventBase.gd
@@ -17,3 +17,12 @@ func _load_scene() -> Node:
 	
 func get_room_abbreviation() -> String:
 	return ""
+
+## What to do once the event starts
+func on_event_started() -> void:
+	PlayerManager.is_map_movement_allowed = false
+	
+## What to do once the event ends
+func on_event_ended() -> void:
+	PlayerManager.is_map_movement_allowed = true
+
