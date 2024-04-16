e1ff3f82c680110ce60a02ac8c9b58f5d942a262
Jona <jona.lam@yahoo.com>
2024-04-17 00:29:13 +0200

    Comented confusing code as well as made functions which aren't used in other scripts private

--- InventoryComponents/InventoryConsumablesComponent.gd
+++ InventoryComponents/InventoryConsumablesComponent.gd
@@ -1,5 +1,6 @@
 class_name InventoryConsumablesComponent
 
+##The held_consumables array has slots with the value null so that we can have item in for exsample slot 1 and 3 but nothing in slot 2
 var _held_consumables : Array[Consumable]
 var _max_consumable_number : int = 4
 
@@ -11,19 +12,23 @@ func _init() -> void:
 	_update_consumable_limit()
 	consumable_slot_update.connect(_update_consumable_limit)
 
+##adds a consumable to the first open slot
 func add_consumable(consumable : Consumable) -> void:
 	var i : int = 0
 	
+	##This sycles through the slots and inserts a consumable into the first open slot it can find
 	for consumable_slot in _held_consumables:
 		if(consumable_slot == null):
-			set_consumable(consumable, i)
+			_set_consumable(consumable, i)
 			break
 		i += 1
 
-func set_consumable(consumable : Consumable, pos : int) -> void:
+##sets a given slot to the given consumable
+func _set_consumable(consumable : Consumable, pos : int) -> void:
 	_held_consumables[pos] = consumable
 	held_consumable_update.emit(consumable, pos)
 
+##empties out the consumable in given slot
 func remove_consumable_at_place(pos : int) -> void:
 	_held_consumables[pos] = null
 	held_consumable_update.emit(null, pos)
