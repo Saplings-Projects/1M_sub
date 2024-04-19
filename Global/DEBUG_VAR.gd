0be3f620c97c60f63d540f1a5cd8c594190f0055
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-04-19 17:44:13 +0200

    Remove the need to add the DEBUG_VAR to a list at the end
    The test now gets the list of const from the script directly
    This just means all debug var should be const

--- Global/DEBUG_VAR.gd
+++ Global/DEBUG_VAR.gd
@@ -4,11 +4,7 @@ extends Node
 ## This is useful for testing and debugging [br]
 ## ! Nothing in this file should be set to true for the release [br]
 ## All variable names should start with DEBUG_ [br]
+## ! All variables should be const [br]
 
 ## Player can move freely on the map without having to finish the current event or move in the range of its normal movement
 const DEBUG_FREE_MOVEMENT: bool = false
-
-## List of all debug variables [br]
-## This is used to run the tests checking that every DEBUG is set to false
-## ! If you add a new DEBUG variable, add it to this list
-const LIST_OF_DEBUG: Array[bool] = [DEBUG_FREE_MOVEMENT]
