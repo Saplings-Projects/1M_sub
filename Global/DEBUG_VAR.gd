0adc407b27cd40b7a53a7d4ac632be9d9071e9f2
Tyler Nutsathitya <tynutsathitya@gmail.com>
2024-04-22 13:48:34 -0700

    Issue-75: Event Dialogue System (#92)
    * Issue-75: Add DialogManager add-on, WIP dialog screen
    
    * Issue-75: Add portraits to dialog

--- Global/DEBUG_VAR.gd
+++ Global/DEBUG_VAR.gd
@@ -8,7 +8,10 @@ extends Node
 ## Player can move freely on the map without having to finish the current event or move in the range of its normal movement
 const DEBUG_FREE_MOVEMENT: bool = false
 
+# Clicking on any room will show the test dialog script, check res://Dialog/test.dialogue
+const DEBUG_TEST_DIALOGUE: bool = false
+
 ## List of all debug variables [br]
 ## This is used to run the tests checking that every DEBUG is set to false
 ## ! If you add a new DEBUG variable, add it to this list
-const LIST_OF_DEBUG: Array[bool] = [DEBUG_FREE_MOVEMENT]
+const LIST_OF_DEBUG: Array[bool] = [DEBUG_FREE_MOVEMENT, DEBUG_TEST_DIALOGUE]
