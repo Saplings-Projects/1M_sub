0d0aa88e875967d430badb75c09b2a0776cbfe1f
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-05-03 21:49:33 +0200

    Remove duplicate comment

--- Global/SignalBus.gd
+++ Global/SignalBus.gd
@@ -1,7 +1,6 @@
 extends Node2D
 ## This is meant for having signals that can come from multiple instance of the same class [br]
 ##
-## Only add signals in this file [br]
 ## This is meant for having signals that can come from multiple instance of the same class [br]
 ## Only add signals in this file [br]
 
