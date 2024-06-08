235e5a47f36296e12cf146602c72db55ce9aaacf
TripleCubes <113813506+TripleCubes@users.noreply.github.com>
2024-06-08 10:18:06 +0700

    Skip event button self delete
    Skip event button will delete itself if DEBUG_SKIP_EVENT is false.
    SkipEventButton is now available in the node selection menu.
    SkipEventButton doesnt need to connect its pressed() signal anymore since
    the _pressed() function is called directly.
    
    Also add SkipEventButton to mob and dialogue scene, altho dialogue scene
    still have bug that prevent any button other than the dialogue button
    from being pressed.

--- #Scenes/Events/skipEventButton.gd
+++ #Scenes/Events/skipEventButton.gd
@@ -1,11 +1,13 @@
 extends Button
+class_name SkipEventButton
 ## Code for the button that allows the player to skip the event in placeholder scenes
 
 ## Enable / Disable the button based on the debug variable DEBUG_SKIP_EVENT
 func _ready() -> void:
-	self.disabled = not DebugVar.DEBUG_SKIP_EVENT
+	if not DebugVar.DEBUG_SKIP_EVENT:
+		queue_free()
 	
 
 ## Activate the event ending function when the button is pressed
-func _on_pressed() -> void:
+func _pressed() -> void:
 	PlayerManager.player_room.room_event.on_event_ended()
