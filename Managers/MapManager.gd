6cf3255d6b519eb2909311a8a064763778721666
Lann <hu.julien.ts4@gmail.com>
2024-06-25 21:19:33 +0200

    Implement map generation rules, add map null check in _ready function

--- Managers/MapManager.gd
+++ Managers/MapManager.gd
@@ -26,6 +26,9 @@ var current_map: MapBase
 ## - Width should be odd (for symmetry + padding reason) [br]
 ## - Width should not increase or decrease by more than 2 per floor (this makes it certain all rooms on the map are accessible [br]
 var map_width_array: Array[int]
+
+## Room events that we do not want to be consecutive
+var no_consecutive_room_event: Array[String] = ["shop", "heal"]
  
 #map_floors_width changes the width of the map's floors
 ## Generates and Populates a map with rooms that have random room types. More in depth algorithms will be added in the future
@@ -65,11 +68,84 @@ func create_map(map_floors_width: Array[int] = map_width_array) -> MapBase:
 
 	return _map as MapBase
 
-## Draws room type randomly, with each type having a set probability
+## Checks if the current room event upholds all map generation rules
+func is_room_event_correct(current_room: RoomBase, map: MapBase = current_map) -> bool:
+	## Rule 0: A room cannot have no event
+	if current_room.room_event == null:
+		return false
+	
+	var current_room_event_name: String = current_room.room_event.get_event_name()
+
+	## Rule 1: No Heal room before half of the map
+	if current_room_event_name == "heal" && current_room.room_position.y < int(map.rooms.size()/2.0):
+		return false
+
+
+	## Rule 2: Cannot have 2 consecutive Heal or Shop rooms
+	# Get the parent rooms' types
+	var _parent_events: Array[String] = []
+	for delta_x: int in [-1, 0, 1]:
+		var _parent_x: int = current_room.room_position.x + delta_x
+		var _parent_y: int = current_room.room_position.y - 1
+		if _parent_y >= 0 && _parent_x >= 0 && _parent_x < map.rooms[_parent_y].size() && map.rooms[_parent_y][_parent_x] != null:
+			_parent_events.append(map.rooms[_parent_y][_parent_x].room_event.get_event_name())
+
+	# Check if current room has the same type as one of its parent room
+	if current_room_event_name in no_consecutive_room_event && current_room_event_name in _parent_events:
+		return false
+
+
+	## Rule 3: There must be at least 2 room types among destinations of Rooms that have 2 or more Paths going out.
+	## Since the events of the rooms on the right of current room ((x+i,y), i=1,2,...) have not been generated yet, [br]
+	## we don't need to take them into account. We only check neighbors on the left: (x-2,y), (x-1,y) and (x,y)
+	## Note: We make sure the two leftmost and rightmost rooms of the floor have different events, [br]
+	## since they could be the only destinations of a room from the previous floor (happens when current floor's size [br]
+	## is smaller or equal than previous)
+
+	var _neighbors_events: Array[String] = []
+	var delta_xs: Array[int] = []
+
+	# If rightmost room of floor
+	if current_room.room_position.x == map.rooms[current_room.room_position.y].size() - 1 or map.rooms[current_room.room_position.y][current_room.room_position.x+1] == null:
+		delta_xs = [-1, 0]
+	else:
+		delta_xs = [-2, -1, 0]
+		
+	# Get the neighbor rooms' types
+	for delta_x: int in delta_xs:
+		var _neighbor_x: int = current_room.room_position.x + delta_x
+		var _neighbor_y: int = current_room.room_position.y
+		if _neighbor_x >= 0 && _neighbor_x < map.rooms[_neighbor_y].size() && map.rooms[_neighbor_y][_neighbor_x] != null:
+			_neighbors_events.append(map.rooms[_neighbor_y][_neighbor_x].room_event.get_event_name())
+
+	# If our current room has no neighbors on the left, ignore this rule
+	if _neighbors_events.size() >= 2:
+		var _unique: Array[String] = []
+
+		# Remove duplicates
+		for event in _neighbors_events:
+			if not _unique.has(event):
+				_unique.append(event)
+		
+		# If there is only one event type among all rooms, redraw event
+		if _unique.size() == 1:
+			return false
+
+
+	## Rule 4: No Heal rooms two floors before Boss
+	if (current_room_event_name == "heal" && current_room.room_position.y == map.rooms.size() - 3):
+		return false
+
+	return true
+
+
+## Draws room type randomly, with each type having a set probability [br]
 ## Algorithm taken from this: https://stackoverflow.com/a/1761646
 func draw_room_type() -> EventBase:
-	# Assumes probabilities add up to 100
-	var _rand_type_number: int = randi_range(0, 99)
+	var sum: int = 0
+	for proba in GlobalVar.EVENTS_PROBABILITIES:
+		sum += proba
+	var _rand_type_number: int = randi_range(0, sum-1)
 
 	for _rand_type_index: int in range(GlobalVar.EVENTS_CLASSIFICATION.size()):
 		if _rand_type_number < GlobalVar.EVENTS_PROBABILITIES[_rand_type_index]:
@@ -81,52 +157,55 @@ func draw_room_type() -> EventBase:
 	return null
 
 ## Assign events to existing rooms with set probabilities.
-func assign_events(current_map: MapBase = current_map) -> void:
+func assign_events(map: MapBase = current_map) -> void:
 	# Scan the whole map, assign events to valid rooms
-	for index_height: int in range(current_map.rooms.size()):
-		for index_width: int in range(current_map.rooms[index_height].size()):
+	for index_height: int in range(map.rooms.size()):
+		for index_width: int in range(map.rooms[index_height].size()):
 			# If not a room, ignore
-			if current_map.rooms[index_height][index_width] == null:
+			if map.rooms[index_height][index_width] == null:
 				continue
 			
-			var _current_room: RoomBase = current_map.rooms[index_height][index_width]
+			var _current_room: RoomBase = map.rooms[index_height][index_width]
 
-			if index_height == current_map.rooms.size() - 2:
+			# Set fixed rooms here
+			if index_height == map.rooms.size() - 2:
 				# Rooms before boss are Heal rooms
 				_current_room.room_event = EventHeal.new()
 
-			elif index_height == current_map.rooms.size() - 1:
+			elif index_height == map.rooms.size() - 1:
 				# Boss room, TO BE ASSIGNED
 				_current_room.room_event = EventMob.new()
 
 			else:
-				_current_room.room_event = draw_room_type()
+				while not is_room_event_correct(_current_room, map):
+					_current_room.room_event = draw_room_type()
 
 ## Create a map with a width array
 
 func _ready() -> void:
-	map_width_array = [1, 3, 5, 7, 9, 11, 9, 7, 5, 3, 1]
-	current_map = create_map()
+	if not is_map_initialized():
+		map_width_array = [1, 3, 5, 7, 9, 11, 9, 7, 5, 3, 1]
+		current_map = create_map()
 
 
-	##### DEBUG #####
-	# Count number of each event
-	if DebugVar.DEBUG_PRINT_EVENT_COUNT:
-		var events: Dictionary = {}
-		var total_nb_rooms: int = 0
-		for index_height: int in range(current_map.rooms.size()):
-			for index_width: int in range(current_map.rooms[index_height].size()):
-				if current_map.rooms[index_height][index_width] == null:
-					continue
-					
-				var event: String = current_map.rooms[index_height][index_width].room_event.get_event_name()
-				if not event in events:
-					events[event] = 0
-				events[event] += 1
-				total_nb_rooms += 1
-		for k:String in events:
-			print("Event " + k + " has "+ str(events[k]) + " rooms")
-		print("Total number of rooms generated: " + str(total_nb_rooms))
+		##### DEBUG #####
+		# Count number of each event
+		if DebugVar.DEBUG_PRINT_EVENT_COUNT:
+			var events: Dictionary = {}
+			var total_nb_rooms: int = 0
+			for index_height: int in range(current_map.rooms.size()):
+				for index_width: int in range(current_map.rooms[index_height].size()):
+					if current_map.rooms[index_height][index_width] == null:
+						continue
+						
+					var event: String = current_map.rooms[index_height][index_width].room_event.get_event_name()
+					if not event in events:
+						events[event] = 0
+					events[event] += 1
+					total_nb_rooms += 1
+			for k:String in events:
+				print("Event " + k + " has "+ str(events[k]) + " rooms. (" + str(float(events[k])*100/total_nb_rooms).pad_decimals(2) + "%)")
+			print("Total number of rooms generated: " + str(total_nb_rooms))
 
 ## checks if the map exists
 
