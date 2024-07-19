9a3eb4b18f26ee6373b4e4692c485d02f65b2021
Jona Lam <89871543+JonaLam@users.noreply.github.com>
2024-07-19 15:30:02 +0200

    Add Block display

--- Entity/Player/block_display.gd
+++ Entity/Player/block_display.gd
@@ -1,12 +1,20 @@
 extends Sprite2D
+class_name block_display
+
+## UI element to display block for an entity[br]
 
 @export var label : Label
 @export var healthComponent : HealthComponent
 
 func _ready() -> void:
 	healthComponent.on_block_changed.connect(change_display)
-	pass
 
+## Changes the number on the display, if it's 0 then the shield is hidden[br]
 func change_display(new_num : int) -> void:
+	
+	if(new_num == 0):
+		visible = false
+	else:
+		visible = true
+	
 	label.text = str(new_num)
-	pass
