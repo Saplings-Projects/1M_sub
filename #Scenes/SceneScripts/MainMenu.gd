6b64712116b7e30587d0a3831876b4518c10fbff
Paper02 <Holacasa898@gmail.com>
2024-08-08 19:18:25 -0400

    Makes the buttons that are hovered be lighted

--- #Scenes/SceneScripts/MainMenu.gd
+++ #Scenes/SceneScripts/MainMenu.gd
@@ -5,6 +5,8 @@ extends Control
 func _ready() -> void:
 	$"Menu buttons/Continue".grab_focus()
 	$"game version".text = ProjectSettings.get_setting("application/config/version")
+	for child in get_node("Menu buttons").get_children():
+			child.mouse_entered.connect(_on_button_hovered.bind(child))
 
 ## What happens when the start button is pressed
 func _on_start_pressed() -> void:
@@ -14,6 +16,8 @@ func _on_start_pressed() -> void:
 		# TODO : load the last scene the player was in
 		pass
 
+func _on_button_hovered(button: TextureButton) -> void:
+	button.grab_focus()
 
 ## Scene to be loaded when option button is pressed
 func _on_options_pressed() -> void:
