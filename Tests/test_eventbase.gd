faf03b6c1275d85dbbedf359703057274b73d373
Lann <hu.julien.ts4@gmail.com>
2024-08-10 22:53:16 +0200

    rewrite tests that depended on Event_classification, testing GlobalEnums.choose_event_from_type instead

--- Tests/test_eventbase.gd
+++ Tests/test_eventbase.gd
@@ -10,15 +10,18 @@ func after_each() -> void:
 	assert_no_new_orphans("Orphans still exist, please free up test resources.")
 
 func test_event_base_array_all_types() -> void:
-	var event0: Resource = GlobalVar.EVENTS_CLASSIFICATION[0]
-	assert_eq(event0, EventRandom)
+	var event0: Resource = GlobalEnums.choose_event_from_type(GlobalEnums.EventType.Random)
+	assert_true(event0 is EventRandom)
 	
-	var event1: Resource = GlobalVar.EVENTS_CLASSIFICATION[1]
-	assert_eq(event1, EventHeal)
+	var event1: Resource = GlobalEnums.choose_event_from_type(GlobalEnums.EventType.Heal)
+	assert_true(event1 is EventHeal)
 	
-	var event2: Resource = GlobalVar.EVENTS_CLASSIFICATION[2]
-	assert_eq(event2, EventMob)
+	var event2: Resource = GlobalEnums.choose_event_from_type(GlobalEnums.EventType.Mob)
+	assert_true(event2 is EventMob)
 	
-	var event3: Resource = GlobalVar.EVENTS_CLASSIFICATION[3]
-	assert_eq(event3, EventShop)
+	var event3: Resource = GlobalEnums.choose_event_from_type(GlobalEnums.EventType.Shop)
+	assert_true(event3 is EventShop)
+	
+	var event4: Resource = GlobalEnums.choose_event_from_type(GlobalEnums.EventType.Dialogue)
+	assert_true(event4 is EventDialogue)
 
