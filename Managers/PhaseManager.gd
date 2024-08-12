c3412d460eb965f773910ce8475d1ad83e808212
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-08-12 17:01:29 +0200

    Rewrite PhaseManager

--- Managers/PhaseManager.gd
+++ Managers/PhaseManager.gd
@@ -7,7 +7,8 @@ extends Node
 
 
 signal on_game_start
-signal on_phase_changed(new_phase: GlobalEnums.Phase, old_phase: GlobalEnums.Phase)
+signal on_combat_phase_changed(new_phase: GlobalEnums.CombatPhase, old_phase: GlobalEnums.CombatPhase)
+signal on_global_phase_changed(new_phase: GlobalEnums.GlobalPhase, old_phase: GlobalEnums.GlobalPhase)
 ## When a player wins the event
 signal on_event_win
 ## When the player is dead (reduced to 0 health)
@@ -15,10 +16,11 @@ signal on_defeat
 
 ## This is a signal is a temporary solution to block removal while #118 and #120 are done
 ## should be removed with those issues
-signal temp_before_phase_changed(new_phase: GlobalEnums.Phase, old_phase: GlobalEnums.Phase)
+signal temp_before_phase_changed(new_phase: GlobalEnums.CombatPhase, old_phase: GlobalEnums.CombatPhase)
 
-var current_phase: GlobalEnums.Phase = GlobalEnums.Phase.NONE
-
+var current_combat_phase: GlobalEnums.CombatPhase = GlobalEnums.CombatPhase.PLAYER_ATTACKING
+var current_combat_phase_index: int = 0
+var current_global_phase: GlobalEnums.GlobalPhase = GlobalEnums.GlobalPhase.NONE
 
 func _ready() -> void:
 	# initialize_game()
@@ -26,7 +28,7 @@ func _ready() -> void:
 	
 
 func initialize_game() -> void:
-	set_phase(GlobalEnums.Phase.GAME_STARTING)
+	set_global_phase(GlobalEnums.GlobalPhase.GAME_STARTING)
 	
 	# TODO give all objects some time to initialize. Kinda hacky
 	await get_tree().create_timer(.1).timeout
@@ -34,18 +36,35 @@ func initialize_game() -> void:
 
 
 func _start_game() -> void:
-	set_phase(GlobalEnums.Phase.PLAYER_ATTACKING)
+	_set_combat_phase(GlobalEnums.CombatPhase.PLAYER_ATTACKING)
 	on_game_start.emit()
 
 
 ## Change phases in the game (mainly used in combat for now)
-func set_phase(phase: GlobalEnums.Phase) -> void:
-	if (current_phase == phase):
-		return
-	
-	var old_phase: GlobalEnums.Phase = current_phase
+func _set_combat_phase(phase: GlobalEnums.CombatPhase) -> void:
+	# allow old phase being same as new phase
+	# this is if you finish a fight on player turn, you start the next also on player turn
+		
+	var old_phase: GlobalEnums.CombatPhase = current_combat_phase
 	
 	temp_before_phase_changed.emit(phase, old_phase)
 	
-	current_phase = phase
-	on_phase_changed.emit(current_phase, old_phase)
+	current_combat_phase = phase
+	on_combat_phase_changed.emit(current_combat_phase, old_phase)
+	
+func start_combat() -> void:
+	current_combat_phase_index = 0
+	_set_combat_phase(GlobalEnums.CombatPhase.values()[0])
+	
+func advance_to_next_combat_phase() -> void:
+	var combat_phase: Array = GlobalEnums.CombatPhase.values()
+	# advance the index by 1, going back to 0 if the index is bigger than the total number of possible phase
+	current_combat_phase_index = (current_combat_phase_index + 1) % (combat_phase.size())
+	_set_combat_phase(GlobalEnums.CombatPhase.values()[current_combat_phase_index])
+	
+func set_global_phase(phase: GlobalEnums.GlobalPhase) -> void:
+	if (current_global_phase == phase):
+		return
+	var old_phase: GlobalEnums.GlobalPhase = current_global_phase
+	current_global_phase = phase
+	on_global_phase_changed.emit(current_global_phase, old_phase)
