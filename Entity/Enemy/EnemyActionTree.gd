e3a1208e25c57ff61f3ff83e3cb9b24d90e07677
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-11-18 18:22:35 +0100

    Add function to choose the next action of an enemy

--- Entity/Enemy/EnemyActionTree.gd
+++ Entity/Enemy/EnemyActionTree.gd
@@ -16,3 +16,21 @@ class_name EnemyActionTree extends Resource
 	load("res://Cards/Resource/Card_Get_block.tres"): NextCardProbability.new(100, load("res://Cards/Resource/Card_Damage.tres"))
 }
 
+var last_chosen_action: CardBase = null
+
+func choose_next_action() -> CardBase:
+	var possible_next_action: Array = action_tree[last_chosen_action]
+	# checking the total of the probabilities
+	var total_probability: int = 0
+	for card_probability: NextCardProbability in possible_next_action:
+		total_probability += card_probability.probability
+	# choose a number between 0 and the total probability -1
+	var random_choice: int = randi_range(0, total_probability -1)
+	var probability_sum: int = 0
+	for card_probability: NextCardProbability in possible_next_action:
+		if probability_sum <= random_choice && random_choice < probability_sum + card_probability.probability:
+			return card_probability.card
+		else:
+			probability_sum += card_probability.probability
+	push_error("The next action choice for the enemy didn't find a suitable next action, returning default")
+	return load("res://Cards/Resource/Card_Damage.tres")
