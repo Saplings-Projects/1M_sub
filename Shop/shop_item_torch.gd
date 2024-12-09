6bf319030f038c01e93638267c1602ef34cf14ad
Jona Lam <89871543+JonaLam@users.noreply.github.com>
2024-12-09 18:01:30 +0100

    Commented code

--- Shop/shop_item_torch.gd
+++ Shop/shop_item_torch.gd
@@ -11,11 +11,16 @@ func _ready() -> void:
 	
 	var startPos : Vector2 = startPosTransform.position
 	
+	## Instansiates the amount of torches held in this item. [br]
+	## Each torch is moved a set distance away from the last
 	for i in amount:
 		var instance : TextureRect = torchTexture.instantiate()
 		heldTorches.append(instance) 
 		instance.position = startPos
-		startPos += Vector2.RIGHT * 5
+		
+		var torch_distance : float = 5
+		
+		startPos += Vector2.RIGHT * torch_distance
 		add_child(instance)
 	
 	super()
