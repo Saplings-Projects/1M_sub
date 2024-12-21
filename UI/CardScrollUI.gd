3434537228206d858bdaeda0bcdcce55824d5ae5
Tyler Nutsathitya <tynutsathitya@gmail.com>
2024-12-21 08:18:01 -0800

    Issue-112: Implement Cards (#138)
    - Add cards: Slap, Double Slap, Banana, Broom, Ara-Ara, Sweep, Mic Bonk, Sneeze, Uuuuu, Whispers, Wink, Wrench Ancient Meme, Fun Facts, Gaslight, Headbutt Attack, Stealth Crouch, Take Cover, Backwards Driving, Blue Dart Gun, Green Dart Gun, Heart Hands, pspspsps, and Vampiric Bite
    
    - Fix Tests, Add Default Art if no art for card

--- UI/CardScrollUI.gd
+++ UI/CardScrollUI.gd
@@ -6,10 +6,13 @@ var cards_to_display: Array[CardBase] = []
 var card_worlds: Array[CardWorld] = []
 var cardworld: CardWorld = null
 var cardui: Control = null
+var key_art: Control = null
 
-const CARD_SCALE: Vector2 = Vector2(.7, .7)
+const KEY_ART_SCALE: Vector2 = Vector2(1, 1)
+const CARD_SCALE: Vector2 = Vector2(1, 1)
 const CARDUI_INDEX: int = 2
-const CARDUI_POS: Vector2 = Vector2(4,4)
+const CARDUI_POS: Vector2 = Vector2(150,275)
+const KEYART_UI_OFFSET: Vector2 = Vector2(50, 130)
 const SIZE_OFFSET: Vector2 = Vector2(10,10)
 
 #deletes the root node CardScrollUI with the on screen button
@@ -42,12 +45,8 @@ func populate(parent_name: String) -> void:
 	for card: CardBase in cards_to_display:
 
 		cardworld = card_scene.instantiate()
-		cardui = cardworld.get_child(CARDUI_INDEX)
-
-		cardworld.custom_minimum_size = (cardui.size * CARD_SCALE) + SIZE_OFFSET
-		cardui.scale = CARD_SCALE
-		cardui.anchors_preset = CORNER_TOP_LEFT
-		cardui.position = CARDUI_POS
+		cardui = cardworld.get_node("CardUI")
+		key_art = cardworld.get_node("KeyArt")
 
 		$ScrollContainer/GridContainer.add_child(cardworld)
 		
